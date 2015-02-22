5252b012cf8b8892b85c38f06d3b359a6971d66f
Noah Holmes <noah.r.holmes@gmail.com>
2015-02-21 19:11:39 -0500

    changes made to seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,11 @@
 u1 = User.create(email: 'user@example.com', password: 'password')
 u2 = User.create(email: 'user2@example.com', password: 'password')
  
-p1 = u1.posts.create(title: 'First Post', body: 'An Airplane')
-p2 = u1.posts.create(title: 'Second Post', body: 'A Train')
+p1 = u1.posts.create(title: 'First Post', body: 'An Airplane Can Fly')
+p2 = u1.posts.create(title: 'Second Post', body: 'A Train Can ChooChoo')
  
-p3 = u2.posts.create(title: 'Third Post', body: 'A Truck')
-p4 = u2.posts.create(title: 'Fourth Post', body: 'A Boat')
+p3 = u2.posts.create(title: 'Third Post', body: 'A Truck Is Nice')
+p4 = u2.posts.create(title: 'Fourth Post', body: 'A Boat Is Nice, Too')
  
-p3.comments.create(body: "This post was terrible", user: u1)
-p4.comments.create(body: "This post was the best thing in the whole world", user: u1)
+p3.comments.create(body: "This post was terrible. GET IT AWAY.", user: u1)
+p4.comments.create(body: "This post was the best thing in the whole world, I wish I could read it again for the first time.", user: u1)
